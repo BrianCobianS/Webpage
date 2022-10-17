0a876232dcc0d16984b9f274b6db52c84638f5c7
BrianCobianS <brian.cobian@alumnos.udg.mx>
2022-10-17 13:02:43 -0500

    cambios en el python

--- Versiones.py
+++ Versiones.py
@@ -1,10 +1,13 @@
-
+from pyparsing import line
+import wget
+import os
+url = 'http://10.89.110.62:28081/service/rest/repository/browse/tgcs-maven-snapshot/com/toshibacommerce/ace/ACE3D001/'
+output_directory = './update'
+filename = wget.download(url , out=output_directory)
+# os.rename('./update/download.wget', './update/x')
 
 def changeBat(fileDirectory):
     with open(fileDirectory,'r') as archivo:
-      i=0
-      j=0
-      pos=[]
       Jedi=[]
       Morty=[]
       Leia=[]
@@ -21,25 +24,48 @@ def changeBat(fileDirectory):
                   Leia.append(linea1)
                 elif 'V8R3' in linea1:
                   Morty.append(linea1)
-
-                pos.append(i)
-                j=j+1
-            i=i+1
       versiones.append(Jedi)
       versiones.append(Leia)
       versiones.append(Morty)
+      complementos=[]
+      for tipo in versiones:
+        for version in tipo:
+          # print(version + '-SNAPSHOT')
+          url = 'http://10.89.110.62:28081/repository/tgcs-maven-snapshot/com/toshibacommerce/ace/ACE3D001/' +version + '-SNAPSHOT/maven-metadata.xml'
+          output_directory = './update'
+          complement = wget.download(url , out=output_directory)
+          with open(complement,'r') as archivo:
+            for linea in archivo:
+              linea= linea.rstrip('\n')
+              if '<value>' in linea:
+                inicio=linea.find('<value>')
+                final=linea.find('</value>')
+                linea1=linea[inicio+7:final]
+                complementos.append(linea1)
+                print(linea1)
+                break
+          os.remove(complement)
+      for a in complementos:
+        print(a)
       Tipo=['Jedi','Leia','Morty']
       contenido = open('Versiones.json', "w")
       contenido.writelines('{\n')
+      i=0
       for x in range(3):
             contenido.writelines('  "'+Tipo[x]+'": [\n')
             f=0
             for index in versiones[x]:
               f += 1
+              print(i)
               if  f == len(versiones[x]) :
-                contenido.writelines('  {\n  "id":'+str(f)+',\n  "nivel": "'+index+'"\n  }\n')
+                contenido.writelines('  {\n  "id":'+str(f)+',\n  "nivel": "'+index+'",\n  "complemento": "'+complementos[i]+'"\n}\n')
               else:
-                contenido.writelines('  {\n  "id":'+str(f)+',\n  "nivel": "'+index+'"\n  },\n')
+                contenido.writelines('  {\n  "id":'+str(f)+',\n  "nivel": "'+index+'",\n  "complemento": "'+complementos[i]+'"\n },\n')
+              i += 1
             contenido.writelines('  ]\n') if x == 2 else contenido.writelines('  ],\n')
+
       contenido.writelines('}\n')
-changeBat('var/www/html/update/nexus')
+# changeBat('var/www/html/update/nexus')
+changeBat(filename)
+os.remove(filename)
+
