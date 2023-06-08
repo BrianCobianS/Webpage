7fd3de0c2f540996e1d59878c9171bc5596d04a6
BrianCobianS <brian.cobian@alumnos.udg.mx>
2023-06-08 12:10:27 -0600

    Updater

--- Versiones.py
+++ Versiones.py
@@ -15,7 +15,13 @@ def verificar_crear_directorio(directorio):
 
 # Ejemplo de uso
 
-
+def organizar(cadena):
+    partes = cadena.split("-")  # Divide la cadena en partes separadas por "-"
+    if len(partes) >= 3:  # Verifica si hay al menos dos caracteres "-" en la cadena
+        return partes[1]  # Devuelve la segunda parte de la cadena (lo que está entre los caracteres "-")
+    else:
+        return None  # Si no hay suficientes caracteres "-", devuelve None
+      
 def eliminar_contenido_directorio(directorio):
     for archivo in os.listdir(directorio):
         ruta_archivo = os.path.join(directorio, archivo)
@@ -108,34 +114,66 @@ ACEBASE=''
 ACEBASE=getverison('3D',ACEBASE)
 getverison('4D',ACEBASE)
 getverison('6D',ACEBASE)
-# print(ACEREALESES)
-# print(ACELIST)
-#Codigo para revisar las diferecias entre la copia local y los de la nube
-# nexus=[]
-# for nivel in ACELIST:
-#     for comple in ACEREALESES:
-#         if (nivel in comple):
-#           nexus.append('ACE'+comple[3:5]+'001'+'-'+comple[9:]+'.zip')
-# # print(nexus)
-# locales=os.listdir('./Packs')
-# # print(locales)
-# faltantes=[]
-# for elemento in nexus:
-#     if elemento not in locales:
-#         faltantes.append(elemento[:-4])
-#         print('Falta: '+elemento[:-4])
-        
 directorio = './Packs'
 verificar_crear_directorio(directorio)
-eliminar_contenido_directorio(directorio)
+# print(ACEREALESES)
+# print(ACELIST)
+# Codigo para revisar las diferecias entre la copia local y los de la nube
+nexus=[]
 for nivel in ACELIST:
-# for nivel in faltantes:
     for comple in ACEREALESES:
         if (nivel in comple):
-          url = 'https://nexus.commerce.toshiba.com/repository/tgcs-maven-group/com/toshibacommerce/ace/ACE'+comple[3:5]+'001/'+nivel+'-SNAPSHOT/ACE'+comple[3:5]+'001-'+comple[9:]+'.zip'
-          output_directory = './Packs'
-          print(url)
-          wget.download(url , out=output_directory)
+          nexus.append('ACE'+comple[3:5]+'001'+'-'+comple[9:]+'.zip')
+# print(nexus)
+locales=os.listdir('./Packs')
+# print(locales)
+faltantes=[]
+for elemento in nexus:
+    if elemento not in locales:
+        faltantes.append(elemento[:-4])
+        # print('Falta: '+elemento[:-4])
+faltantes=list(set(faltantes))
+CloudVersionsAce =[]
+for elemento in faltantes:
+  CloudVersionsAce.append(organizar(elemento))
+  # print(organizar(elemento))
+
+
+if (len(faltantes)>=1): 
+  for prined in faltantes:
+    print('Falta: '+prined)
+  for x in range(len(faltantes)):
+    url = 'https://nexus.commerce.toshiba.com/repository/tgcs-maven-group/com/toshibacommerce/ace/ACE'+faltantes[x][3:5]+'001/'+CloudVersionsAce[x]+'-SNAPSHOT/ACE'+faltantes[x][3:5]+'001-'+faltantes[x][9:]+'.zip'
+    output_directory = './Packs'
+    print(url)
+    try:
+      wget.download(url , out=output_directory)
+    except Exception as e:
+      print("Ocurrió un error:", str(e))
+else:
+  print('Ya esta actualizada la base de datos')
+
+
+
+
+
+# directorio = './Packs'
+# verificar_crear_directorio(directorio)
+# # eliminar_contenido_directorio(directorio)
+
+# for nivel in CloudVersionsAce:
+# # for nivel in faltantes:
+#     for comple in ACEREALESES:
+#         if (nivel in comple):
+#           url = 'https://nexus.commerce.toshiba.com/repository/tgcs-maven-group/com/toshibacommerce/ace/ACE'+comple[3:5]+'001/'+nivel+'-SNAPSHOT/ACE'+comple[3:5]+'001-'+comple[9:]+'.zip'
+#           output_directory = './Packs'
+#           print(nivel)
+#           print(comple)
+#           print(url)
+#           try:
+#             wget.download(url , out=output_directory)
+#           except Exception as e:
+#             print("Ocurrió un error:", str(e))
           
 # El archivo que se genera de 6D es el que contiene la informacion util para la aplicacion porque son las versiones que existen tanto 
 #en EPS como en SUREPOS ACE para JEDI y para morty y Leia son las mismas que son iguales entre en el archivo de ACE 3D Y 4D
